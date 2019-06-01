e1a5abae1145ad374948a143f68fe39e3c181a02
Satyadeep <satyadeep.1991@gmail.com>
2019-06-02 01:30:41 +0530

    If the user signed up through any login provider, activate automatically without email verification

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,12 @@ use Illuminate\Contracts\Events\Dispatcher;
 
 return function (Dispatcher $events) {
     $events->listen(Registered::class, function (Registered $event) {
-        $event->user->activate();
-        $event->user->save();
+        $user = $event->user;
+        $loginProviders = $user->loginProviders();
+        //If the user signed up through any login provider, activate automatically without email verification
+        if(!empty($loginProviders) && sizeof($loginProviders) > 0){
+            $user->activate();
+            $user->save();
+        }
     });
 };
