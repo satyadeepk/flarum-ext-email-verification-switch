f9896da683cc169998b384099f2fdfda12aafb89
Satyadeep <satyadeep.1991@gmail.com>
2019-06-02 01:50:28 +0530

    Fix loginProviders call

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Events\Dispatcher;
 return function (Dispatcher $events) {
     $events->listen(Registered::class, function (Registered $event) {
         $user = $event->user;
-        $loginProviders = $user->loginProviders();
+        $loginProviders = $user->loginProviders;
         //If the user signed up through any login provider, activate automatically without email verification
         if(!empty($loginProviders) && sizeof($loginProviders) > 0){
             $user->activate();
